14d0e163eb74e5516dd2649368a9f764bb871a79
Zwubman <zwubeman@gmail.com>
2025-04-30 02:34:19 -0400

    fix the router

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,17 +1,26 @@
-import React from 'react';
-import NavBar from './components/NavBar';
-import PictureSight from './components/PictureSight';
-import Footer from './components/Footer';
-import SignupForm from './components/SignupForm';
-import LogIn from './components/LogIn';
+import React from "react";
+import {
+  Route,
+  createBrowserRouter,
+  createRoutesFromElements,
+  RouterProvider,
+} from "react-router-dom";
+import MainLayout from "./layout/MainLayout";
+import HomePage from "./pages/HomePage";
+// import NavBar from "./components/NavBar";
+// import PictureSight from "./components/PictureSight";
+// import Footer from "./components/Footer";
+// import SignupForm from "./components/SignupForm";
+// import LogIn from "./components/LogIn";
+
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path="/" element={<MainLayout />}>
+      <Route index element={<HomePage />} />
+    </Route>
+  )
+);
 
 export default function App() {
-  return (
-    <div>
-        {/* <NavBar />
-        <PictureSight />
-        <Footer /> */}
-        <LogIn />
-    </div>
-  )
+  return <RouterProvider router={router} />;
 }
---
+++ frontend/src/pages/HomePage.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import PictureSight from '../components/PictureSight'
+import Footer from '../components/Footer'
+
+const HomePage = () => {
+  return (
+    <>
+        <PictureSight />
+        <Footer />
+    </>
+  )
+}
+
+export default HomePage
